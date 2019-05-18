d03c8ad9a505b9517026f294f10dea5e0bb4703a
MaurizioVacca <mauriziovacca@easy-network.it>
2019-05-18 18:26:30 +0200

    Add Card stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,9 +5,12 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Welcome } from '@storybook/react/demo';
-import { Button } from 'shared';
+import { Button, Card } from 'shared';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
-    .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>);
+    .add('basic', () => <Button onClick={action('clicked')}>Hello Button</Button>);
+
+storiesOf('Card', module)
+    .add('basic', () => <Card>This is a basic card</Card>);
