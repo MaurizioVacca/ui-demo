713e45b62b29ed168f71e4be4b25d8da8ab08ffc
MaurizioVacca <mauriziovacca@easy-network.it>
2019-05-19 10:07:15 +0200

    Add data mock and api

---
+++ src/api/articles.js
@@ -0,0 +1,5 @@
+import articleData from './mock';
+
+const getArticles = () => articleData;
+
+export default getArticles;
---
+++ src/api/mock.js
@@ -0,0 +1,31 @@
+const articles = [{
+    id: 1,
+    icon: '',
+    caption: 'Nun ce sta\' mammà là dint\'!',
+    title: 'Quann steva là in Honduras, steva rint a na capanna e nziemm a me ce steva.',
+    description: 'E intant\' e sord tuoj nunn arrivavan. \'Na sera gli honduregni me mettetter\' nu macete n\'man e gridavan\': \'Accirel\'! Accirel\'! Je pregav\', pregav\' ca corcrun\' me venev\' a salva\', ca tu me veniv\' a salva\'! ',
+    hasDetails: false
+}, {
+    id: 2,
+    icon: 'pig',
+    caption: 'Sapeva pure ca oggi je e te stevema inda \'sta machina',
+    title: 'Quann\' tu me purtast\' a spara\' chillu cristian\' p\'a prima vota.',
+    description: '',
+    hasDetails: true
+}, {
+    id: 3,
+    icon: '',
+    caption: '',
+    title: 'Quann\' Salvatore Conte è turnat\', quann\' Danielino è muort\', iss \'o sapeva.',
+    description: '',
+    hasDetails: false
+}, {
+    id: 4,
+    icon: '',
+    caption: '',
+    title: 'Mammà se n\'è ghiuta! Tu ci credi in Dio?',
+    description: '',
+    hasDetails: false
+}];
+
+export default articles;
