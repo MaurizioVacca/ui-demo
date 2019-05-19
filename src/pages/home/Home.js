88a589e4f6f4905ef652ccf6270b5af45e2fc3f3
MaurizioVacca <mauriziovacca@easy-network.it>
2019-05-19 20:10:22 +0200

    Add NavBar animations, pass highlights to article

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,73 @@
-import React, { Fragment } from 'react';
+import React, {
+    Fragment,
+    useState,
+    useEffect
+} from 'react';
 
-import { Cover, NavBar } from 'sections';
+import { Article, NavBar } from 'sections';
 
 import HomeCover from 'images/cover.png';
+import getArticles from 'api/articles';
 
-const Home = () => (
-    <Fragment>
-        <NavBar />
-        <Cover imgPath={HomeCover} />
-    </Fragment>
-);
+const Home = () => {
+    const [articles, setArticles] = useState([])
+    const [showNavBar, setShowNavBar] = useState(true);
+    const articleRef = React.createRef();
+    const navBarRef = React.createRef();
+
+    const fetchData = () => {
+        let remoteData = null;
+
+        if (!remoteData) {
+            remoteData = getArticles();
+
+            setArticles(remoteData);
+        }
+    };
+
+    const animateNavBar = () => {
+        const checkNavbarDistanceFromContent = () => {
+            const { current: navBarCurrent } = navBarRef;
+            const { current: articleCurrent } = articleRef;
+
+            const navbarPosition = navBarCurrent.getBoundingClientRect();
+            const articlePosition = articleCurrent.getBoundingClientRect();
+
+            const vDistance = articlePosition.y - (navbarPosition.y + navbarPosition.height);
+
+            if (vDistance <= 36 && showNavBar) {
+                setShowNavBar(false);
+            }
+
+            if (vDistance > 36 && !showNavBar) {
+                setShowNavBar(true);
+            }
+        };
+
+        window.addEventListener('scroll', checkNavbarDistanceFromContent);
+
+        return () => window.removeEventListener('scroll', checkNavbarDistanceFromContent);
+    };
+
+    useEffect(fetchData,[]);
+    useEffect(animateNavBar);
+
+    const highlights = articles.filter((article, index) => index > 0);
+    const article = articles[0];
+
+    return (
+        <Fragment>
+            <NavBar ref={navBarRef} show={showNavBar} />
+            {article && (
+                <Article
+                    cover={HomeCover}
+                    article={article}
+                    ref={articleRef}
+                    highlights={highlights}
+                />
+            )}
+        </Fragment>
+    );
+};
 
 export default Home;
