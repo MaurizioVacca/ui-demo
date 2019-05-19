08738d28fffea8a55a8bd95cdb4037a45ae99140
MaurizioVacca <mauriziovacca@easy-network.it>
2019-05-19 17:48:52 +0200

    Add Article container animation

--- src/sections/article/Article.js
+++ src/sections/article/Article.js
@@ -1,20 +1,65 @@
-import React from 'react';
+import React, {
+    useRef,
+    useEffect,
+    useState
+} from 'react';
 import PropTypes from 'prop-types';
 
+import { articleShape } from 'models';
+
 import Cover from '../cover';
-import { ArticleWrapper, ArticleCoverWrapper } from './Styled';
+import {
+    ArticleAnimatedBackground,
+    ArticleBody,
+    ArticleCaption,
+    ArticleHeading,
+    ArticleWrapper,
+    ArticleCoverWrapper,
+    ArticleContent
+} from './Styled';
 
 const propTypes = {
-    cover: PropTypes.string.isRequired
+    cover: PropTypes.string.isRequired,
+    article: articleShape.isRequired
 };
 
-const Article = ({ cover }) => (
-    <ArticleWrapper>
-        <ArticleCoverWrapper>
-            <Cover imgPath={cover} />
-        </ArticleCoverWrapper>
-    </ArticleWrapper>
-);
+const Article = ({ cover, article }) => {
+    const [backgroundScale, setBackgroundScale] = useState(1);
+
+    const animatedBgRef = useRef(null);
+
+    useEffect(() => {
+        const checkArticlePosition = () => {
+            const offsetTop = animatedBgRef.current.getBoundingClientRect().top;
+
+            if (offsetTop <= 0 && backgroundScale === 1) {
+                setBackgroundScale(1.9);
+            }
+
+            if (offsetTop > 0 && backgroundScale > 1) {
+                setBackgroundScale(1);
+            }
+        };
+
+        window.addEventListener('scroll', checkArticlePosition);
+
+        return () => window.removeEventListener('scroll', checkArticlePosition);
+    }, [animatedBgRef, backgroundScale]);
+
+    return (
+        <ArticleWrapper>
+            <ArticleCoverWrapper>
+                <Cover imgPath={cover} />
+            </ArticleCoverWrapper>
+            <ArticleContent>
+                <ArticleAnimatedBackground ref={animatedBgRef} xScale={backgroundScale} />
+                <ArticleCaption>{article.caption}</ArticleCaption>
+                <ArticleHeading>{article.title}</ArticleHeading>
+                <ArticleBody>{article.description}</ArticleBody>
+            </ArticleContent>
+        </ArticleWrapper>
+    );
+};
 
 Article.propTypes = propTypes;
 
--- src/sections/article/Styled.js
+++ src/sections/article/Styled.js
@@ -6,7 +6,44 @@ const ArticleWrapper = styled.div(() => ({
     width: '100%'
 }));
 
-const ArticleContent = styled.div(() => ({ }));
+const ArticleHeading = styled.h1(({ theme }) => ({
+    ...theme.text.h1,
+    zIndex: 10,
+    position: 'relative'
+}));
+
+const ArticleBody = styled.div(({ theme }) => ({
+    ...theme.text.paragraph,
+    zIndex: 10,
+    position: 'relative'
+}));
+
+const ArticleCaption = styled.div(({ theme }) => ({
+    ...theme.text.caption,
+    zIndex: 10,
+    position: 'relative'
+}));
+
+const ArticleContent = styled.div(() => ({
+    position: 'relative',
+    top: -188,
+    width: 976,
+    margin: '0 auto',
+    padding: '42px 98px'
+}));
+
+const ArticleAnimatedBackground = styled.div(({ theme, xScale = 1 }) => ({
+    backgroundColor: theme.colors.white,
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    zIndex: 1,
+    top: 0,
+    left: 0,
+    transition: 'transform 0.4s ease-in-out',
+    borderRadius: '12px 12px 0 0',
+    transform: `scale(${xScale}, 1)`
+}));
 
 const ArticleCoverWrapper = styled.div(() => ({
     position: 'relative',
@@ -20,7 +57,11 @@ const ArticleCoverWrapper = styled.div(() => ({
 }));
 
 export {
+    ArticleBody,
+    ArticleCaption,
     ArticleContent,
+    ArticleAnimatedBackground,
     ArticleCoverWrapper,
+    ArticleHeading,
     ArticleWrapper
 };
