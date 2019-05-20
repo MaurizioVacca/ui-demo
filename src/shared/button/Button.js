22a582a4aac6b29ae065970674c09402f2d7f7ce
MaurizioVacca <mauriziovacca@easy-network.it>
2019-05-20 23:42:35 +0200

    Remove outline

--- src/shared/button/Button.js
+++ src/shared/button/Button.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 const Button = styled.button(({ theme, color = 'primary' }) => ({
-    ...theme.buttons[color]
+    ...theme.buttons[color],
+    outline: 0
 }));
 
 export default Button;
