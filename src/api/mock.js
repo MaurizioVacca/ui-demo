dfb6cbe24289eae6545464dd653ad48a2e14a274
MaurizioVacca <mauriziovacca@easy-network.it>
2019-05-20 20:14:46 +0200

    Add images to articles mock

--- src/api/mock.js
+++ src/api/mock.js
@@ -1,5 +1,12 @@
 import avatarPath from 'images/263ec435b123bbb5318e70fe81700254a940d9a7.png';
 
+import pic1 from 'images/5e994a78aee1fb16342014976380cead345d648c.png';
+import pic2 from 'images/3d704f7e5e067856183c00912fca82303c560c1b.png';
+import pic3 from 'images/faf9cb11335f63a521c0a18c88286ab92ef6bbcc.png';
+import pic4 from 'images/b4e075b4267562d209f7f2df0afa6c4719b431b2.png';
+import pic5 from 'images/231e2d44418e0daedd3caaf8db1aeab6413dfa4a.png';
+import pic6 from 'images/c98c54b4d824d7327c8f4eca96ef04a50031e537.png';
+
 const articles = [{
     id: 1,
     icon: '',
@@ -7,7 +14,15 @@ const articles = [{
     title: 'Quann steva là in Honduras, steva rint a na capanna e nziemm a me ce steva.',
     description: 'E intant\' e sord tuoj nunn arrivavan. \'Na sera gli honduregni me mettetter\' nu macete n\'man e gridavan\': \'Accirel\'! Accirel\'! Je pregav\', pregav\' ca corcrun\' me venev\' a salva\', ca tu me veniv\' a salva\'! ',
     hasDetails: false,
-    author: 1
+    author: 1,
+    images: [
+        pic1,
+        pic2,
+        pic3,
+        pic4,
+        pic5,
+        pic6
+    ]
 }, {
     id: 2,
     icon: 'pig',
@@ -15,7 +30,14 @@ const articles = [{
     title: 'Quann\' tu me purtast\' a spara\' chillu cristian\' p\'a prima vota.',
     description: '',
     hasDetails: true,
-    author: 1
+    author: 1,
+    images: [
+        pic4,
+        pic4,
+        pic4,
+        pic4,
+        pic4
+    ]
 }, {
     id: 3,
     icon: '',
@@ -23,7 +45,8 @@ const articles = [{
     title: 'Quann\' Salvatore Conte è turnat\', quann\' Danielino è muort\', iss \'o sapeva.',
     description: '',
     hasDetails: false,
-    author: 1
+    author: 1,
+    images: []
 }, {
     id: 4,
     icon: '',
@@ -31,7 +54,8 @@ const articles = [{
     title: 'Mammà se n\'è ghiuta! Tu ci credi in Dio?',
     description: '',
     hasDetails: false,
-    author: 1
+    author: 1,
+    images: []
 }];
 
 const users = [{
@@ -41,4 +65,4 @@ const users = [{
     avatar: avatarPath
 }];
 
-export { users, articles};
+export { users, articles };
