52678a2df2db4fa01ac0dfdf6e472cbb25f34ac5
MaurizioVacca <mauriziovacca@easy-network.it>
2019-05-21 20:38:07 +0200

    Add modal component

---
+++ src/shared/modal/ModalContent.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const ModalContent = styled.div(() => ({
+    position: 'relative',
+    width: 1170
+}));
+
+export default ModalContent;
---
+++ src/shared/modal/ModalOverlay.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const ModalOverlay = styled.div(() => ({
+    width: '100vw',
+    height: '100vh',
+    zIndex: 30,
+    position: 'fixed',
+    top: 0,
+    left: 0
+}));
+
+export default ModalOverlay;
