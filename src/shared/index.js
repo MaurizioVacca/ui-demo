f807d59abe984c37b12760a8702f661b71257add
MaurizioVacca <mauriziovacca@easy-network.it>
2019-05-21 20:38:15 +0200

    Export modal

--- src/shared/index.js
+++ src/shared/index.js
@@ -4,14 +4,16 @@ import Logo from './logo';
 import Spacer from './spacer';
 import { Grid, Column, Cell } from './grid';
 import Icon from './icon';
+import Modal from './modal';
 
 export {
     Button,
     Card,
+    Cell,
     Column,
     Grid,
     Icon,
     Logo,
-    Cell,
+    Modal,
     Spacer
 };
